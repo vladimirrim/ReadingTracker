506ec0c5985c61e251095e01da829b86cd8a6b6b
vladimirrim <vladimirrim98@gmail.com>
2018-10-04 23:51:02 +0300

    Add skeleton for info gathering in sign up,disable horizontal orientation.

---
+++ app/src/main/java/ru/hse/egorov/reading_tracker/adapter/SignUpInfoAdapter.kt
@@ -0,0 +1,23 @@
+package ru.hse.egorov.reading_tracker.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class SignUpInfoAdapter : RecyclerView.Adapter<SignUpInfoAdapter.SignUpInfoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SignUpInfoViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: SignUpInfoViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class SignUpInfoViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        //TODO
+    }
+}
