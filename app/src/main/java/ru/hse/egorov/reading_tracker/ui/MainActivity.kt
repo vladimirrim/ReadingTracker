f1b54514a44290c4b8cce724618850aa229a2ffd
vladimirrim <vladimirrim98@gmail.com>
2018-10-08 15:47:59 +0300

    Add popup menu to main activity with buttons to library and book adding.

--- app/src/main/java/ru/hse/egorov/reading_tracker/ui/MainActivity.kt
+++ app/src/main/java/ru/hse/egorov/reading_tracker/ui/MainActivity.kt
@@ -1,11 +1,15 @@
 package ru.hse.egorov.reading_tracker.ui
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.PopupMenu
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.hse.egorov.reading_tracker.R
+import ru.hse.egorov.reading_tracker.ui.book_library.AddingBookActivity
+import ru.hse.egorov.reading_tracker.ui.book_library.BookLibraryActivity
 import ru.hse.egorov.reading_tracker.ui.session.ReadingSessionActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +21,34 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ReadingSessionActivity::class.java)
             startActivity(intent)
         }
+
+        popupMenuButton.setOnClickListener {
+            showPopupMenu()
+        }
+    }
+
+    private fun showPopupMenu() {
+        val popup = PopupMenu(this, popupMenuButton)
+        popup.menuInflater
+                .inflate(R.menu.main_activity_menu, popup.menu)
+
+        popup.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.toLibrary -> {
+                    val intent = Intent(this, BookLibraryActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.toBookAdding -> {
+                    val intent = Intent(this, AddingBookActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        popup.show()
     }
 }
