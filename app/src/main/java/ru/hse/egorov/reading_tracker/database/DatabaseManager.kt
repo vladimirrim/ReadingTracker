2544b9f3c42dd3002835b012b1f9a34df74a36bc
vladimirrim <vladimirrim98@gmail.com>
2018-10-16 00:20:09 +0300

    Add info gathering at the end of the session.

--- app/src/main/java/ru/hse/egorov/reading_tracker/database/DatabaseManager.kt
+++ app/src/main/java/ru/hse/egorov/reading_tracker/database/DatabaseManager.kt
@@ -4,7 +4,9 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 
 
 class DatabaseManager {
@@ -13,8 +15,8 @@ class DatabaseManager {
         return authManager?.createUserWithEmailAndPassword(email, password)
     }
 
-    fun addUserInfo(info: Map<String, Any?>): Task<DocumentReference> {
-        return db.collection("users").add(info)
+    fun addUserInfo(info: Map<String, Any?>): Task<Void> {
+        return db.collection("users").document(authManager.uid as String).set(info)
     }
 
     fun isAuth(): Boolean {
@@ -25,8 +27,15 @@ class DatabaseManager {
         return authManager?.signInWithEmailAndPassword(email, password)
     }
 
+    fun addSession(time:String): Task<Void> {
+        val map = HashMap<String,List<String>>()
+        map["sessions"] = listOf(time)
+        return db.collection("users").document(authManager.uid as String).update("sessions",
+                FieldValue.arrayUnion("${System.currentTimeMillis()} $time"))
+    }
+
     companion object {
-        private val db = FirebaseFirestore.getInstance()
         private val authManager = FirebaseAuth.getInstance()
+        private val db = FirebaseFirestore.getInstance(authManager.app)
     }
 }
--- app/src/main/java/ru/hse/egorov/reading_tracker/ui/session/EndOfSessionActivity.kt
+++ app/src/main/java/ru/hse/egorov/reading_tracker/ui/session/EndOfSessionActivity.kt
@@ -3,11 +3,15 @@ package ru.hse.egorov.reading_tracker.ui.session
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_end_of_session.*
 import ru.hse.egorov.reading_tracker.R
+import ru.hse.egorov.reading_tracker.database.DatabaseManager
 import ru.hse.egorov.reading_tracker.ui.MainActivity
 
 class EndOfSessionActivity : AppCompatActivity() {
+
+    private val db = DatabaseManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,11 @@ class EndOfSessionActivity : AppCompatActivity() {
         val time = intent.getStringExtra(SESSION_TIME)
         val text = "Session time:\n$time"
         sessionTime.text = text
+
+        db.addSession(time).addOnSuccessListener {
+            Toast.makeText(this, "OK!",
+                    Toast.LENGTH_SHORT).show()
+        }
 
         homeButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
