08fde237d1fb4ed11b9d7bf002f974afc4ee58eb
vladimirrim <vladimirrim98@gmail.com>
2018-11-07 22:54:31 +0300

    Add progress bar in Sign Up screen.

--- app/src/main/java/ru/hse/egorov/reading_tracker/database/DatabaseManager.kt
+++ app/src/main/java/ru/hse/egorov/reading_tracker/database/DatabaseManager.kt
@@ -21,7 +21,7 @@ class DatabaseManager {
     }
 
     fun isAuth(): Boolean {
-        return false//authManager?.currentUser != null
+        return authManager?.currentUser != null
     }
 
     fun signIn(email: String, password: String): Task<AuthResult>? {
--- app/src/main/java/ru/hse/egorov/reading_tracker/ui/login/SignUpEmailActivity.kt
+++ app/src/main/java/ru/hse/egorov/reading_tracker/ui/login/SignUpEmailActivity.kt
@@ -5,11 +5,17 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up_email.*
 import ru.hse.egorov.reading_tracker.R
 import ru.hse.egorov.reading_tracker.database.DatabaseManager
 import ru.hse.egorov.reading_tracker.ui.MainActivity
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
+
+
 
 /**
  * A login screen that offers login via email/password.
@@ -23,15 +29,17 @@ class SignUpEmailActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = actionBarTitle
-
-        signInEmail.setOnClickListener {
+        signUpEmail.setOnClickListener {
             if (checkAllFieldsFilled()) {
+                showProgressBar()
                 dbManager.auth(email.text.toString(), password.text.toString())?.addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         Log.d(TAG, "createUserWithEmail:success")
+                        hideProgressBar()
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                     } else {
+                        hideProgressBar()
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
                         Snackbar.make(findViewById(android.R.id.content),
                                 "Authentication failed.Reason:${task.exception?.localizedMessage}",
@@ -44,6 +52,23 @@ class SignUpEmailActivity : AppCompatActivity() {
                         Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun showProgressBar(){
+        val animation = RotateAnimation(0.0f, 360.0f,
+                Animation.RELATIVE_TO_SELF, 0.49f, Animation.RELATIVE_TO_SELF,
+                0.53f)
+        animation.repeatCount = -1
+        animation.duration = 1000
+        animation.interpolator = LinearInterpolator()
+        signUpEmail.visibility = View.INVISIBLE
+        progressBar.startAnimation(animation)
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar(){
+        progressBar.visibility = View.INVISIBLE
+        signUpEmail.visibility = View.VISIBLE
     }
 
     private fun checkAllFieldsFilled(): Boolean {
