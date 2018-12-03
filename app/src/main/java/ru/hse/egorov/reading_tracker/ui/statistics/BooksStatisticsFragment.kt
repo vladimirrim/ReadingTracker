34512377b18646ddc250424cfe55142752ae6b78
vladimirrim <vladimirrim98@gmail.com>
2018-12-03 16:13:00 +0300

    Create book statistics fragment.

--- app/src/main/java/ru/hse/egorov/reading_tracker/ui/statistics/BooksStatisticsFragment.kt
+++ app/src/main/java/ru/hse/egorov/reading_tracker/ui/statistics/BooksStatisticsFragment.kt
@@ -2,9 +2,11 @@ package ru.hse.egorov.reading_tracker.ui.statistics
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_books_statistics.view.*
 import ru.hse.egorov.reading_tracker.R
 
 class BooksStatisticsFragment:Fragment() {
@@ -12,6 +14,16 @@ class BooksStatisticsFragment:Fragment() {
                               savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_books_statistics, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpBooks(view)
+    }
+
+    private fun setUpBooks(view:View){
+        view.books.layoutManager = LinearLayoutManager(context)
+        //TODO
+    }
 
     companion object {
         /**
